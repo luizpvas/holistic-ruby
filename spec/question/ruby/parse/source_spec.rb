ab016585e1839818af765d2f4372308091e18067
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-02-25 10:54:29 -0300

    wrap spec in .call context

--- spec/question/ruby/parse/source_spec.rb
+++ spec/question/ruby/parse/source_spec.rb
@@ -1,129 +1,131 @@
 # frozen_string_literal: true
 
 describe ::Question::Ruby::Parse::Source do
-  describe "module declaration in the global namespace" do
-    subject(:parse_source) { described_class.call(application:, source:) }
+  describe ".call" do
+    context "module declaration in the global namespace" do
+      subject(:parse_source) { described_class.call(application:, source:) }
 
-    let(:application) { ::Question::Ruby::Application.new }
+      let(:application) { ::Question::Ruby::Application.new }
 
-    let(:source) do
-      <<-RUBY
-      module MyModule
-        Name.foo()
-      end
-      RUBY
-    end
-
-    it "finishes the parsing process in global namespace" do
-      parse_source
-
-      expect(application.constant_repository.namespace.global?).to be(true)
-    end
-
-    it "stores a constant reference for `Name`" do
-      parse_source
-
-      references = application.constant_repository.references
-
-      expect(references.size).to eql(1)
-      expect(references.first.name).to eql("Name")
-      expect(references.first.namespace.name).to eql("MyModule")
-    end
-  end
-
-  describe "module declaration in global scope with double colon syntax" do
-    subject(:parse_source) { described_class.call(application:, source:) }
-
-    let(:application) { ::Question::Ruby::Application.new }
-
-    let(:source) do
-      <<-RUBY
-      module MyApp::MyModule
-        Name.foo()
-      end
-      RUBY
-    end
-
-    it "finishes the parsing process in global namespace" do
-      parse_source
-
-      expect(application.constant_repository.namespace.global?).to be(true)
-    end
-
-    it "stores a constant reference for `Name`" do
-      parse_source
-
-      references = application.constant_repository.references
-
-      expect(references.size).to eql(1)
-      expect(references.first.name).to eql("Name")
-      expect(references.first.namespace.name).to eql("MyApp::MyModule")
-    end
-  end
-
-  describe "module declaration with nested syntax" do
-    subject(:parse_source) { described_class.call(application:, source:) }
-
-    let(:application) { ::Question::Ruby::Application.new }
-
-    let(:source) do
-      <<-RUBY
-      module MyApp
+      let(:source) do
+        <<-RUBY
         module MyModule
           Name.foo()
         end
+        RUBY
       end
-      RUBY
+
+      it "finishes the parsing process in global namespace" do
+        parse_source
+
+        expect(application.constant_repository.namespace.global?).to be(true)
+      end
+
+      it "stores a constant reference for `Name`" do
+        parse_source
+
+        references = application.constant_repository.references
+
+        expect(references.size).to eql(1)
+        expect(references.first.name).to eql("Name")
+        expect(references.first.namespace.name).to eql("MyModule")
+      end
     end
 
-    it "finishes the parsing process in global namespace" do
-      parse_source
+    context "module declaration in global scope with double colon syntax" do
+      subject(:parse_source) { described_class.call(application:, source:) }
 
-      expect(application.constant_repository.namespace.global?).to be(true)
-    end
+      let(:application) { ::Question::Ruby::Application.new }
 
-    it "stores a constant reference for `Name`" do
-      parse_source
-
-      references = application.constant_repository.references
-
-      expect(references.size).to eql(1)
-      expect(references.first.name).to eql("Name")
-      expect(references.first.namespace.name).to eql("MyModule")
-      expect(references.first.namespace.parent.name).to eql("MyApp")
-    end
-  end
-
-  describe "module declaration with nested syntax AND double colon syntax" do
-    subject(:parse_source) { described_class.call(application:, source:) }
-
-    let(:application) { ::Question::Ruby::Application.new }
-
-    let(:source) do
-      <<-RUBY
-      module MyApp
-        module MyModule1::MyModule2
+      let(:source) do
+        <<-RUBY
+        module MyApp::MyModule
           Name.foo()
         end
+        RUBY
       end
-      RUBY
+
+      it "finishes the parsing process in global namespace" do
+        parse_source
+
+        expect(application.constant_repository.namespace.global?).to be(true)
+      end
+
+      it "stores a constant reference for `Name`" do
+        parse_source
+
+        references = application.constant_repository.references
+
+        expect(references.size).to eql(1)
+        expect(references.first.name).to eql("Name")
+        expect(references.first.namespace.name).to eql("MyApp::MyModule")
+      end
     end
 
-    it "finishes the parsing process in global namespace" do
-      parse_source
+    context "module declaration with nested syntax" do
+      subject(:parse_source) { described_class.call(application:, source:) }
 
-      expect(application.constant_repository.namespace.global?).to be(true)
+      let(:application) { ::Question::Ruby::Application.new }
+
+      let(:source) do
+        <<-RUBY
+        module MyApp
+          module MyModule
+            Name.foo()
+          end
+        end
+        RUBY
+      end
+
+      it "finishes the parsing process in global namespace" do
+        parse_source
+
+        expect(application.constant_repository.namespace.global?).to be(true)
+      end
+
+      it "stores a constant reference for `Name`" do
+        parse_source
+
+        references = application.constant_repository.references
+
+        expect(references.size).to eql(1)
+        expect(references.first.name).to eql("Name")
+        expect(references.first.namespace.name).to eql("MyModule")
+        expect(references.first.namespace.parent.name).to eql("MyApp")
+      end
     end
 
-    it "stores a constant reference for `Name`" do
-      parse_source
+    context "module declaration with nested syntax AND double colon syntax" do
+      subject(:parse_source) { described_class.call(application:, source:) }
 
-      references = application.constant_repository.references
+      let(:application) { ::Question::Ruby::Application.new }
 
-      expect(references.size).to eql(1)
-      expect(references.first.name).to eql("Name")
-      expect(references.first.namespace.name).to eql("MyModule1::MyModule2")
-      expect(references.first.namespace.parent.name).to eql("MyApp")
+      let(:source) do
+        <<-RUBY
+        module MyApp
+          module MyModule1::MyModule2
+            Name.foo()
+          end
+        end
+        RUBY
+      end
+
+      it "finishes the parsing process in global namespace" do
+        parse_source
+
+        expect(application.constant_repository.namespace.global?).to be(true)
+      end
+
+      it "stores a constant reference for `Name`" do
+        parse_source
+
+        references = application.constant_repository.references
+
+        expect(references.size).to eql(1)
+        expect(references.first.name).to eql("Name")
+        expect(references.first.namespace.name).to eql("MyModule1::MyModule2")
+        expect(references.first.namespace.parent.name).to eql("MyApp")
+      end
     end
   end
 end
