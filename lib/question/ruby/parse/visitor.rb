2fead73decfb3c7dcf6149b8502ecb861c9d8f16
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-05-08 21:34:16 -0300

    wrap helpers in module

--- lib/question/ruby/parse/visitor.rb
+++ lib/question/ruby/parse/visitor.rb
@@ -2,37 +2,39 @@
 
 module Question::Ruby::Parse
   module Visitor
-    DeclarationName = ->(node) do
-      parts = []
+    module Node
+      GetDeclarationName = ->(node) do
+        parts = []
 
-      append = ->(node) do
-        case node
-        when ::SyntaxTree::ConstRef     then parts << node.child_nodes[0].value
-        when ::SyntaxTree::Const        then parts << node.value
-        when ::SyntaxTree::VarRef       then node.child_nodes.each(&append)
-        when ::SyntaxTree::ConstPathRef then node.child_nodes.each(&append)
-        else raise "Unexpected node type: #{node.class}"
+        append = ->(node) do
+          case node
+          when ::SyntaxTree::ConstRef     then parts << node.child_nodes[0].value
+          when ::SyntaxTree::Const        then parts << node.value
+          when ::SyntaxTree::VarRef       then node.child_nodes.each(&append)
+          when ::SyntaxTree::ConstPathRef then node.child_nodes.each(&append)
+          else raise "Unexpected node type: #{node.class}"
+          end
         end
+
+        append.(node) and parts.join("::")
       end
 
-      append.(node) and parts.join("::")
-    end
-
-    BuildSourceLocation = ->(node) do
-      ::Question::Ruby::SourceLocation.new(
-        file_path: nil,
-        start_line: node.location.start_line,
-        end_line: node.location.end_line
-      )
+      BuildSourceLocation = ->(node) do
+        ::Question::Ruby::SourceLocation.new(
+          file_path: nil,
+          start_line: node.location.start_line,
+          end_line: node.location.end_line
+        )
+      end
     end
 
     class ProgramVisitor < ::SyntaxTree::Visitor
       visit_methods do
         def visit_module(node)
           declaration, statements = node.child_nodes
-          declaration_name = DeclarationName[declaration]
+          declaration_name = Node::GetDeclarationName[declaration]
 
-          source_location = BuildSourceLocation.call(node)
+          source_location = Node::BuildSourceLocation.call(node)
 
           repository.open_module(name: declaration_name, source_location:) { visit(statements) }
         end
@@ -41,11 +43,11 @@ module Question::Ruby::Parse
           declaration, superclass, statements = node.child_nodes
 
           if superclass
-            repository.add_reference!(DeclarationName[superclass])
+            repository.add_reference!(Node::GetDeclarationName[superclass])
           end
 
-          declaration_name = DeclarationName[declaration]
-          source_location = BuildSourceLocation.call(node)
+          declaration_name = Node::GetDeclarationName[declaration]
+          source_location = Node::BuildSourceLocation.call(node)
 
           repository.open_class(name: declaration_name, source_location:) { visit(statements) }
         end
