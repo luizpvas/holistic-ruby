d42f047fd302adce75c82be7283f38195a1a9d78
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-05-13 12:06:42 -0300

    server static files

--- lib/question/http_application.rb
+++ lib/question/http_application.rb
@@ -5,6 +5,8 @@ require "sinatra"
 class Question::HttpApplication < ::Sinatra::Base
   before { content_type :json }
 
+  set :public_folder, ::File.expand_path("../../web-client/dist", __dir__)
+
   get "/health_check" do
     { status: "ok" }.to_json
   end
