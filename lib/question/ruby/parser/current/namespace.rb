ec9a39614cedfad9e4c3ef69d16c495af1a23824
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-05-20 11:52:22 -0300

    remove todo comment

--- lib/question/ruby/parser/current/namespace.rb
+++ lib/question/ruby/parser/current/namespace.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 module Question::Ruby::Parser
-  # TODO: The tree structure should have one record per module/class. Even with the collapsed
-  # syntax (e.g. `MyApp::MyModule`) we should end up with two records where `MyModule` is
-  # a child of `MyApp`.
   module Current::Namespace
     extend self
 
