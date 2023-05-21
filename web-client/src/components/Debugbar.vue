48ef707233d57c99691478a8b8883500d0c4e742
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-05-21 11:29:07 -0300

    add tailwind to the project

--- web-client/src/components/Debugbar.vue
+++ web-client/src/components/Debugbar.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        {{ currentApplicationName }}
-    </div>
+  <div
+    class="absolute top-2 right-2 bg-slate-800 text-white text-xs px-2 py-1 rounded-md divide-x"
+  >
+    <span title="Current application" class="pr-2">
+      {{ currentApplicationName }}
+    </span>
+
+    <span title="Amount of applications" class="pl-2">
+      {{ applications.length }}
+    </span>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { currentApplicationName } from '../models/application';
+import { applications, currentApplicationName } from "../models/application";
 </script>
