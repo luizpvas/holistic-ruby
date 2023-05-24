051711ec15b6bf1eb241ba7d0e61dd83bc1c10cf
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-05-24 09:14:22 -0300

    add search controller

--- lib/question/controllers/search_controller.rb
+++ lib/question/controllers/search_controller.rb
@@ -3,9 +3,20 @@
 module Question::Controllers::SearchController
   extend ::ActiveSupport::Concern
 
+  Serialize = ->(match) do
+    {
+      uuid: match.document.uuid,
+      text: match.document.text,
+      highlighted_text: match.highlighted_text,
+      score: match.score
+    }
+  end
+
   included do
     get "/applications/:application_name/search" do
-      
+      application = ::Question::Ruby::Application::Repository.find(params[:application_name])
+
+      application.symbol_index.search(params[:query]).map(&Serialize).to_json
     end
   end
 end
