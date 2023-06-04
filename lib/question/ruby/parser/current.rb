532e10908322123fa1d59fae280f2ceabd9d3f31
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-04 00:46:01 -0300

    add comment about attribute name

--- lib/question/ruby/parser/current.rb
+++ lib/question/ruby/parser/current.rb
@@ -6,6 +6,7 @@ require "active_support/current_attributes"
 
 module Question::Ruby::Parser
   class Current < ::ActiveSupport::CurrentAttributes
+    # TODO: Is `resolution` the best name here? Maybe something with scope? It's used for constant resolution, but it's the current scope. I dunno.
     attribute :application, :namespace, :resolution, :file_path
   end
 end
