242b07dc6271800a11f5350e6a15490f91aaadb0
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-08 11:28:34 -0300

    add file change spec

--- spec/question/ruby/parser/live_editing/process_file_changed_spec.rb
+++ spec/question/ruby/parser/live_editing/process_file_changed_spec.rb
@@ -30,6 +30,47 @@ describe ::Question::Ruby::Parser::LiveEditing::ProcessFileChanged do
   end
 
   context "when file content is different" do
-    it "deletes symbols from previous content and parses the new one"
+    let(:application) do
+      parse_snippet <<~RUBY
+        module MyApp
+          module Example1; end
+        end
+      RUBY
+    end
+
+    let(:new_source_code) do
+      <<~RUBY
+      module MyApp
+        module Example2; end
+      end
+      RUBY
+    end
+
+    before do
+      expect(application.files.find("snippet.rb"))
+        .to receive(:read)
+        .and_return(new_source_code)
+    end
+
+    it "deletes symbols from previous content and parses new ones" do
+      my_app_before = application.symbol_index.find("::MyApp")
+      my_app_example_1_before = application.symbol_index.find("::MyApp::Example1")
+      my_app_example_2_before = application.symbol_index.find("::MyApp::Example2")
+
+      described_class.call(application:, file: application.files.find("snippet.rb"))
+
+      my_app_after = application.symbol_index.find("::MyApp")
+      my_app_example_1_after = application.symbol_index.find("::MyApp::Example1")
+      my_app_example_2_after = application.symbol_index.find("::MyApp::Example2")
+
+      expect(my_app_before.identifier).to eql(my_app_after.identifier)
+      expect(my_app_before).not_to be(my_app_after)
+
+      expect(my_app_example_1_before).to be_a(::Question::Ruby::Symbol::Record)
+      expect(my_app_example_1_after).to be_nil
+
+      expect(my_app_example_2_before).to be_nil
+      expect(my_app_example_2_after).to be_a(::Question::Ruby::Symbol::Record)
+    end
   end
 end
