e56fde4416b81d854a37caf53c9b016f96e0d979
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-08 21:58:41 -0300

    add specs for source location identifier

--- lib/question/source_code/location.rb
+++ lib/question/source_code/location.rb
@@ -8,5 +8,7 @@ module Question::SourceCode
     :end_line,
     :end_column,
     keyword_init: true
-  )
+  ) do
+    def identifier = "#{file_path}[#{start_line},#{start_column},#{end_line},#{end_column}]"
+  end
 end
