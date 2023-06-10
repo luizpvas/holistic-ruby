cf902b67d9f9ed348a340ce59f6f60de34e12afe
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-10 13:57:15 -0300

    add spec for unknown namespace reference

--- spec/question/ruby/type_inference/solve/solving_namespace_reference_spec.rb
+++ spec/question/ruby/type_inference/solve/solving_namespace_reference_spec.rb
@@ -37,5 +37,23 @@ describe ::Question::Ruby::TypeInference::Solve do
   end
 
   context "when the clue is a namespace reference and the namespace does not exist" do
+    let(:application) do
+      parse_snippet <<~RUBY
+      module MyApp
+        Unknown.call
+      end
+      RUBY
+    end
+
+    it "leaves the conclusion empty" do
+      symbols = application.symbol_index.list_symbols_of(kind: :type_inference)
+
+      expect(symbols.size).to eql(1)
+
+      expect(symbols.first.record).to have_attributes(
+        itself: be_a(::Question::Ruby::TypeInference::Something),
+        conclusion: be_nil
+      )
+    end
   end
 end
