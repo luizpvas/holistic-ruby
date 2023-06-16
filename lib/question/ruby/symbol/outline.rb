6265e6ae410c4ac95c23e6d6ece4ba65ea5cd9fc
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-16 11:37:58 -0300

    draft outline spec

---
+++ lib/question/ruby/symbol/outline.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Question::Ruby::Symbol
+  module Outline
+    extend self
+
+    Result = ::Struct.new(
+      :declarations,
+      :dependencies,
+      :dependants,
+      keyword_init: true
+    )
+
+    def call(symbol:)
+      raise ::ArgumentError, "#{symbol.inspect} must be a Symbol" unless symbol.is_a?(Record)
+
+      Result.new(
+        declarations: [],
+        dependencies: [],
+        dependants: []
+      )
+    end
+  end
+end
