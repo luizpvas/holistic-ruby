b70914a52487c9969b653aa3ca7b36c937368ff0
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-18 13:12:01 -0300

    add failing spec for outline module with dependencies

---
+++ spec/holistic/ruby/symbol/outline/outlines_a_module_with_dependencies_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Ruby::Symbol::Outline do
+  include ::SnippetParser
+
+  let(:application) do
+    parse_snippet <<~RUBY
+    module MyApp
+      PlusOne = ->(x) { x + 1 }
+      PlusTwo = ->(x) { x + 2 }
+
+      module Example
+        PlusThree = ->(x) { x + 3 }
+
+        PlusOne.call(1)
+        PlusTwo.call(2)
+        PlusThree.call(3)
+      end
+    end
+    RUBY
+  end
+
+  it "outlines a module with dependencies declared outside of the outlined module" do
+    result = described_class.call(application:, symbol: application.symbols.find("::MyApp::Example"))
+
+    expect(result.references).to be_empty
+    expect(result.dependants).to be_empty
+
+    expect(result.declarations.map(&:identifier)).to match_array([
+      "::MyApp::Example",
+      "::MyApp::Example::PlusThree"
+    ])
+
+    expect(result.dependencies.map(&:identifier)).to match_array([
+      "::MyApp::PlusOne",
+      "::MyApp::PlusTwo"
+    ])
+  end
+end
