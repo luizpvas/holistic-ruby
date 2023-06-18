d01a8fdfbf0ba39cfbb77c88efcfb1100e6afeeb
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-17 23:01:13 -0300

    add todo comment

--- lib/holistic/ruby/symbol/outline.rb
+++ lib/holistic/ruby/symbol/outline.rb
@@ -15,6 +15,7 @@ module Holistic::Ruby::Symbol
     def call(application:, symbol:)
       references = application.dependencies.list_references(dependency_identifier: symbol.identifier)
 
+      # TODO: dedup & add specs
       dependants = references.map { |symbol| symbol.record.namespace }
 
       Result.new(
