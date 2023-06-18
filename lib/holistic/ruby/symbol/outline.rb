2e43b795b1d1c0f32bf9bd71007ea8bb48be9ccd
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-18 11:30:18 -0300

    wip outline declarations

--- lib/holistic/ruby/symbol/outline.rb
+++ lib/holistic/ruby/symbol/outline.rb
@@ -13,12 +13,31 @@ module Holistic::Ruby::Symbol
     )
 
     def call(application:, symbol:)
+      declarations = []
+
+      if symbol.record.is_a?(::Holistic::Ruby::Namespace::Record)
+        namespace = symbol.record
+
+        namespace.children.each do |child_namespace|
+          declarations << application.symbols.find(child_namespace.fully_qualified_name)
+
+          namespace.source_locations.each do |source_location|
+            symbols = application.symbols.list_symbols_in_file(source_location.file_path)
+
+            declarations_of_namespace =
+              symbols.filter { _1.kind == :declaration && _1.record.namespace == namespace }
+
+            declarations.concat(declarations_of_namespace)
+          end
+        end
+      end
+
       references = application.dependencies.list_references(dependency_identifier: symbol.identifier)
 
       dependants = references.map { |symbol| symbol.record.namespace }.uniq
 
       Result.new(
-        declarations: [],
+        declarations:,
         dependencies: [],
         references:,
         dependants:
