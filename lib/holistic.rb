67653de38f69b13190e35348a4012fad9f37f208
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-23 21:08:00 -0300

    add `Holistic.logger`

--- lib/holistic.rb
+++ lib/holistic.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require "logger"
 require "zeitwerk"
 require "syntax_tree"
 
 loader = ::Zeitwerk::Loader.for_gem
 loader.setup
 
-module Holistic; end
+module Holistic
+  extend self
+
+  @logger = ::Logger.new("/home/luiz.vasconcellos/holistic.log")
+  attr_reader :logger
+end
 
 loader.eager_load
