b63eac9cdd3dc3fa6d3ea07d9ec661c37109b0b1
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-25 23:51:33 -0300

    add spec for shutdown message

---
+++ spec/holistic/language_server/requests/shutdown_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+describe ::Holistic::LanguageServer::Requests::Shutdown do
+  let(:message) do
+    ::Holistic::LanguageServer::Message.new({
+      "method" => "shutdown",
+      "jsonrpc" => "2.0",
+      "id" => 3
+    })
+  end
+
+  it "responds with null" do
+    response = described_class.call(message)
+
+    expect(response).to have_attributes(
+      itself: ::Holistic::LanguageServer::Response,
+      result: nil
+    )
+  end
+end
