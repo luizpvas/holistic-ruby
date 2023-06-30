6ab9a512e0c68ba33e930f0953bb0f7a62be5b12
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-06-30 17:01:20 -0300

    add error response kind

--- lib/holistic/language_server/response.rb
+++ lib/holistic/language_server/response.rb
@@ -16,6 +16,22 @@ module Holistic::LanguageServer
       end
     end
 
+    Error = ::Data.define(:message_id, :code, :message, :data) do
+      def encode
+        encoded_payload = {
+          "jsonrpc" => Protocol::JSONRPC_VERSION,
+          "id" => message_id,
+          "error" => {
+            "code" => code,
+            "message" => message,
+            "data" => data
+          }
+        }.to_json
+
+        "#{Protocol::CONTENT_LENGTH_HEADER}:#{encoded_payload.bytesize}#{Protocol::END_OF_HEADER}#{encoded_payload}"
+      end
+    end
+
     Exit = ::Data.define
     NotFound = ::Data.define
 
--- spec/holistic/language_server/response_spec.rb
+++ spec/holistic/language_server/response_spec.rb
@@ -1,17 +1,46 @@
 # frozen_string_literal: true
 
 describe ::Holistic::LanguageServer::Response do
-  describe "#encode" do
-    it "serializes the response as json" do
-      message = ::Holistic::LanguageServer::Message.new({ "jsonrpc" => "2.0", "id" => 15 })
+  describe "Success" do
+    describe "#encode" do
+      it "serializes the response as json" do
+        message = ::Holistic::LanguageServer::Message.new({ "jsonrpc" => "2.0", "id" => 15 })
 
-      response = described_class.in_reply_to(message).with(result: { "example" => "ok" })
+        response = described_class.in_reply_to(message).with(result: { "example" => "ok" })
 
-      encoded_payload = { "jsonrpc" => "2.0", "id" => 15, "result" => { "example" => "ok" } }.to_json
+        encoded_payload = { "jsonrpc" => "2.0", "id" => 15, "result" => { "example" => "ok" } }.to_json
 
-      encoded_response = "Content-Length:#{encoded_payload.bytesize}\r\n\r\n#{encoded_payload}"
+        encoded_response = "Content-Length:#{encoded_payload.bytesize}\r\n\r\n#{encoded_payload}"
 
-      expect(response.encode).to eql(encoded_response)
+        expect(response.encode).to eql(encoded_response)
+      end
+    end
+  end
+
+  describe "Error" do
+    describe "#encode" do
+      it "serializes the response as json" do
+        response = described_class::Error.new(
+          message_id: 15,
+          code: -10,
+          message: "something went wrong",
+          data: { status: "error" }
+        )
+
+        encoded_payload = {
+          "jsonrpc" => "2.0",
+          "id" => 15,
+          "error" => {
+            "code" => -10,
+            "message" => "something went wrong",
+            "data" => { "status" => "error" }
+          }
+        }.to_json
+
+        encoded_response = "Content-Length:#{encoded_payload.bytesize}\r\n\r\n#{encoded_payload}"
+
+        expect(response.encode).to eql(encoded_response)
+      end
     end
   end
 end
