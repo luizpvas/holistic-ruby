0a5589456c11dbd1e6b8e3fc8e4ea23201e625e3
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-07-05 16:42:18 -0300

    draft ad-hoc database implementation

---
+++ lib/holistic/database/table.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+class Holistic::Database::Table
+  RecordNotUniqueError = ::Class.new(::StandardError)
+
+  PRIMARY_KEY = :identifier
+
+  attr_reader :indices
+
+  def initialize(indices: [])
+    @indices =
+      indices.append(PRIMARY_KEY).map do |attribute_name|
+        [attribute_name, ::Hash.new { |hash, key| hash[key] = ::Set.new }]
+      end.to_h
+  end
+
+  def insert(data)
+    primary_key = data.fetch(PRIMARY_KEY)
+
+    if primary_key_index.key?(primary_key)
+      raise RecordNotUniqueError, "record already inserted: #{data.inspect}"
+    end
+
+    indices.each do |attribute_name, index_data|
+      Array(data[attribute_name]).each do |value|
+        index_data[value].add(data)
+      end
+    end
+  end
+
+  def find(identifier)
+    return unless primary_key_index.key?(identifier)
+
+    primary_key_index[identifier].first
+  end
+
+  def filter(attribute_name, value)
+    return unless indices[attribute_name].key?(value)
+
+    indices.dig(attribute_name, value).to_a
+  end
+
+  def delete(identifier)
+    return unless primary_key_index.key?(identifier)
+
+    record = primary_key_index[identifier].first
+
+    indices.each do |attribute_name, index_data|
+      Array(record[attribute_name]).each do |value|
+        index_data[value].delete(record)
+        index_data.delete(value) if index_data[value].empty?
+      end
+    end
+
+    record
+  end
+
+  def count
+    primary_key_index.size
+  end
+
+  private
+
+  def primary_key_index
+    @indices[PRIMARY_KEY]
+  end
+end
---
+++ spec/holistic/database/table_spec.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Database::Table do
+  let(:table) do
+    described_class.new(indices: [:color])
+  end
+
+  describe "#insert" do
+    it "inserts the record" do
+      expect(table.count).to be(0)
+
+      table.insert({ identifier: "example" })
+
+      expect(table.count).to be(1)
+    end
+
+    it "inserts multiple records" do
+      expect(table.count).to be(0)
+
+      table.insert({ identifier: "example1" })
+      table.insert({ identifier: "example2" })
+      table.insert({ identifier: "example3" })
+      
+      expect(table.count).to be(3)
+    end
+
+    it "inserts an entry for each indexed attribute" do
+      record = { identifier: "example", color: "green" }
+
+      table.insert(record)
+
+      expect(table.indices[:identifier]).to include("example")
+      expect(table.indices[:color]).to include("green")
+
+      expect(table.find("example")).to eql(record)
+      expect(table.filter(:color, "green")).to eql([record])
+    end
+
+    it "inserts multiple entries in the index if value is an array" do
+      record = { identifier: "example", color: ["green", "blue"] }
+
+      table.insert(record)
+
+      expect(table.indices[:color]).to include("green")
+      expect(table.indices[:color]).to include("blue")
+
+      expect(table.filter(:color, "green")).to eql([record])
+      expect(table.filter(:color, "blue")).to eql([record])
+    end
+
+    context "when record does not have a primary key" do
+      it "raises an error" do
+        expect { table.insert({}) }.to raise_error(::KeyError)
+      end
+    end
+
+    context "when record with the same primary key already exists" do
+      it "raises an error" do
+        table.insert({ identifier: "example" })
+
+        expect { table.insert({ identifier: "example" }) }.to raise_error(described_class::RecordNotUniqueError)
+      end
+    end
+  end
+
+  describe "#delete" do
+    context "when record exists" do
+      it "returns the deleted data" do
+        table.insert({ identifier: "example" })
+
+        result = table.delete("example")
+
+        expect(result).to eql({ identifier: "example" })
+
+        result = table.delete("example")
+
+        expect(result).to be_nil
+
+        expect(table.count).to be(0)
+      end
+
+      it "deletes the record from all indices" do
+        table.insert({ identifier: "example", color: "green" })
+        table.delete("example")
+
+        expect(table.indices[:identifier]).to be_empty
+        expect(table.indices[:color]).to be_empty
+      end
+
+      it "deletes the record from all indices when value is an array" do
+        table.insert({ identifier: "example", color: ["green", "blue"] })
+        table.delete("example")
+
+        expect(table.indices[:identifier]).to be_empty
+        expect(table.indices[:color]).to be_empty
+      end
+    end
+
+    context "when record does not exist" do
+      it "returns nil" do
+        result = table.delete("non-existing-primary-key")
+
+        expect(result).to be_nil
+      end
+    end
+  end
+end
