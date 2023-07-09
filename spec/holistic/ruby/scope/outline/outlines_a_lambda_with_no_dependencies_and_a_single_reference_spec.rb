e19819953690d0044bcea178f3a7ebdac38ba602
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-07-09 18:25:27 -0300

    move outline from symbol to scope

--- spec/holistic/ruby/scope/outline/outlines_a_lambda_with_no_dependencies_and_a_single_reference_spec.rb
+++ spec/holistic/ruby/scope/outline/outlines_a_lambda_with_no_dependencies_and_a_single_reference_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe ::Holistic::Ruby::Symbol::Outline do
+describe ::Holistic::Ruby::Scope::Outline do
   include ::Support::SnippetParser
 
   let(:application) do
