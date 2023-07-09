7ae49fa461b3319f030966f158c7840ff5f79fdd
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-07-08 23:56:35 -0300

    remove outdated docs

--- lib/holistic/ruby/parser/current/registration_queue.rb
+++ lib/holistic/ruby/parser/current/registration_queue.rb
@@ -1,27 +1,6 @@
 # frozen_string_literal: true
 
 class Holistic::Ruby::Parser::Current
-  # Parsing is done in four phases.
-  #
-  # 1. Register scopes, contants, declarations, lambdas, etc. as they are encountered in source code.
-  # 2. Update symbol index.
-  # 3. Run type inference algorithm.
-  # 4. Update dependency graph.
-  #
-  # This class is responsible to handle the steps after the first one. If you're interested in the first phase,
-  # take a look at the `Visitor` class.
-  #
-  # At this point, the source of truth is good. We have scopes, constants, declarations, lambdas, etc. in their specialized
-  # types related to one another.
-  #
-  # ## When to accumulate and when to process?
-  #
-  # In the boot process (Holistic::Cli::Start) we need to parse all files in the project *and then* process them. This is because
-  # dependency graph and type inference needs all definitions available. We can't process them as we go.
-  #
-  # After the initial boot, we have a similar situation for file changes, but this time at a smaller scale. The visitor registers
-  # symbols as it goes and then we can process the queue for that particular file. This is because the rest of the project remains
-  # the same.
   class RegistrationQueue
     attr_reader :application
 
