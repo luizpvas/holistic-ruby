ea0e0a2def8553cba3978efb260a06c30e7e4276
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-07-12 22:11:42 -0300

    extract lambda to const

--- lib/holistic/ruby/scope/list_references.rb
+++ lib/holistic/ruby/scope/list_references.rb
@@ -4,20 +4,20 @@ module Holistic::Ruby::Scope
   module ListReferences
     extend self
 
+    QueryReferencesRecursively = ->(application, scope) do
+      references_to_scope = application.references.list_references_to(scope.fully_qualified_name)
+
+      references_to_child_scopes = scope.children.flat_map { QueryReferencesRecursively.call(application, _1) }
+
+      references_to_scope + references_to_child_scopes
+    end
+
     def call(application:, cursor:)
       scope = application.scopes.find_by_cursor(cursor)
 
       return :not_found if scope.nil?
 
-      query_references_recursively = ->(scope) do
-        references = application.references.list_references_to(scope.fully_qualified_name)
-
-        references_to_child_scopes = scope.children.flat_map { query_references_recursively.call(_1) }
-
-        references + references_to_child_scopes
-      end
-
-      references = query_references_recursively.call(scope)
+      references = QueryReferencesRecursively.call(application, scope)
 
       [:references_listed, {references:}]
     end
