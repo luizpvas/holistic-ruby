7ce200418c58d5104a0e7a023dad40f18b8a3715
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-07-29 10:23:15 -0300

    use same method for consistency

--- lib/holistic/language_server/requests/text_document/did_change.rb
+++ lib/holistic/language_server/requests/text_document/did_change.rb
@@ -4,14 +4,14 @@ module Holistic::LanguageServer
   module Requests::TextDocument::DidChange
     extend self
 
-    BuildChange = ->(language_server_change) do
+    BuildDocumentChange = ->(params) do
       ::Holistic::Document::Unsaved::Change.new(
-        range_length: language_server_change["rangeLength"],
-        text: language_server_change["text"],
-        start_line: language_server_change.dig("range", "start", "line"),
-        start_column: language_server_change.dig("range", "start", "character"),
-        end_line: language_server_change.dig("range", "end", "line"),
-        end_column: language_server_change.dig("range", "end", "character")
+        range_length: params.dig("rangeLength"),
+        text: params.dig("text"),
+        start_line: params.dig("range", "start", "line"),
+        start_column: params.dig("range", "start", "character"),
+        end_line: params.dig("range", "end", "line"),
+        end_column: params.dig("range", "end", "character")
       )
     end
 
@@ -20,7 +20,7 @@ module Holistic::LanguageServer
 
       unsaved_document = request.application.unsaved_documents.find(file_path)
 
-      request.param("contentChanges").map(&BuildChange).each do |change|
+      request.param("contentChanges").map(&BuildDocumentChange).each do |change|
         unsaved_document.apply_change(change)
       end
 
