8c1f005d24b671c72f814df84e93cd7f598e531a
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-08-17 16:21:38 -0300

    add spec for registering method call clue to local variable

---
+++ spec/holistic/ruby/type_inference/clue/method_call/method_call_on_local_variable_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Ruby::TypeInference::Clue::MethodCall do
+  include ::Support::SnippetParser
+
+  context "when calling a method on a local variable" do
+    let(:application) do
+      parse_snippet <<~RUBY
+      user.nickname
+      RUBY
+    end
+
+    it "registers the method call clue" do
+      reference = application.references.find_by_code_content("user.nickname")
+
+      expect(reference.clues.size).to be(1)
+      expect(reference.clues.first).to have_attributes(
+        itself: be_a(::Holistic::Ruby::TypeInference::Clue::MethodCall),
+        nesting: ::Holistic::Ruby::Parser::NestingSyntax.new("user"),
+        method_name: "nickname",
+        resolution_possibilities: ["::"]
+      )
+    end
+  end
+end
