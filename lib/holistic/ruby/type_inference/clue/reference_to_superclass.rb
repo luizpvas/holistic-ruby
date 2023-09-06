fd048b0e91ec68d4e5efa9db95cdee4bdd7389ae
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-09-05 23:43:39 -0300

    use Data.define instead of struct

--- lib/holistic/ruby/type_inference/clue/reference_to_superclass.rb
+++ lib/holistic/ruby/type_inference/clue/reference_to_superclass.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 module Holistic::Ruby::TypeInference::Clue
-  ReferenceToSuperclass = ::Struct.new(
-    :subclass_scope,
-    keyword_init: true
-  ) do
+  ReferenceToSuperclass = ::Data.define(:subclass_scope) do
     def to_s
       nesting.to_s
     end
