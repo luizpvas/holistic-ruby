c979def125ffc306d17d1a02730cfef35de1cd16
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-09-06 00:04:13 -0300

    fix to_s implementation

--- lib/holistic/ruby/type_inference/clue/reference_to_superclass.rb
+++ lib/holistic/ruby/type_inference/clue/reference_to_superclass.rb
@@ -3,7 +3,7 @@
 module Holistic::Ruby::TypeInference::Clue
   ReferenceToSuperclass = ::Data.define(:subclass_scope) do
     def to_s
-      nesting.to_s
+      "superclass of #{subclass_scope.fully_qualified_name}"
     end
   end
 end
