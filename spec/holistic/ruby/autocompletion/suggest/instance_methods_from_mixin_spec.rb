b447b85a634284a0ca2c0341a313bea13269cc64
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-09-07 13:50:20 -0300

    add spec autocompletion mixin

---
+++ spec/holistic/ruby/autocompletion/suggest/instance_methods_from_mixin_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Ruby::Autocompletion::Suggest do
+  concerning :Helpers do
+    include ::Support::SnippetParser
+
+    def assert_suggestions(code, suggestions)
+      scope = application.scopes.find("::MyApp::Child#child_method")
+
+      actual_suggestions = described_class.call(code:, scope:)
+
+      expected_suggestions = suggestions.map do |suggestion|
+        described_class::Suggestion.new(code: suggestion[:code], kind: suggestion[:kind])
+      end
+
+      expect(actual_suggestions).to eql(expected_suggestions)
+    end
+  end
+
+  let(:application) do
+    parse_snippet <<~RUBY
+    module MyApp
+      module Parent_1
+        def parent_1_method
+        end
+      end
+
+      module Parent_2
+        def parent_2_method
+        end
+      end
+
+      class Child
+        include Parent_1
+        include Parent_2
+
+        def child_method
+          # autocomplete here
+        end
+      end
+    end
+    RUBY
+  end
+
+  it "suggests p" do
+    assert_suggestions("p", [
+      { code: "parent_1_method", kind: :instance_method },
+      { code: "parent_2_method", kind: :instance_method }
+    ])
+  end
+end
---
+++ spec/holistic/ruby/autocompletion/suggest/instance_methods_from_parent_class_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Ruby::Autocompletion::Suggest do
+  concerning :Helpers do
+    include ::Support::SnippetParser
+
+    def assert_suggestions(code, suggestions)
+      scope = application.scopes.find("::MyApp::Child#child_method")
+
+      actual_suggestions = described_class.call(code:, scope:)
+
+      expected_suggestions = suggestions.map do |suggestion|
+        described_class::Suggestion.new(code: suggestion[:code], kind: suggestion[:kind])
+      end
+
+      expect(actual_suggestions).to eql(expected_suggestions)
+    end
+  end
+
+  let(:application) do
+    parse_snippet <<~RUBY
+    module MyApp
+      class Parent
+        def parent_method
+        end
+
+        def overriden_method
+        end
+      end
+
+      class Child < Parent
+        def child_method
+          # autocomplete here
+        end
+
+        def overriden_method
+        end
+      end
+    end
+    RUBY
+  end
+
+  it "suggests p" do
+    assert_suggestions("p", [
+      { code: "parent_method", kind: :instance_method }
+    ])
+  end
+
+  it "suggests o" do
+    assert_suggestions("o", [
+      { code: "overriden_method", kind: :instance_method }
+    ])
+  end
+end
