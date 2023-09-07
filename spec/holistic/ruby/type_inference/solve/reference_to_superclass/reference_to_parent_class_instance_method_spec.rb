6bfc3e2bad5f738d687e2ffc388d1ee7bcd2dc9a
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-09-07 09:52:35 -0300

    add spec for grantparent class method lookup

--- spec/holistic/ruby/type_inference/solve/reference_to_superclass/reference_to_parent_class_instance_method_spec.rb
+++ spec/holistic/ruby/type_inference/solve/reference_to_superclass/reference_to_parent_class_instance_method_spec.rb
@@ -25,4 +25,29 @@ describe ::Holistic::Ruby::TypeInference::Solve do
       expect(parent_method_call.referenced_scope.fully_qualified_name).to eql("::Parent#parent_method")
     end
   end
+
+  context "when calling methods defined in the grandparent class" do
+    let(:application) do
+      parse_snippet <<~RUBY
+      class Grandparent
+        def grandparent_method
+        end
+      end
+
+      class Parent < Grandparent; end
+
+      class Child < Parent
+        def child_method
+          grandparent_method
+        end
+      end
+      RUBY
+    end
+
+    it "solves the method call reference" do
+      parent_method_call = application.references.find_by_code_content("grandparent_method")
+
+      expect(parent_method_call.referenced_scope.fully_qualified_name).to eql("::Grandparent#grandparent_method")
+    end
+  end
 end
