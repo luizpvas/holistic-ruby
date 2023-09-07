06e399f5d9f9bb5c20aa51f3e6e9d100abb260bb
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-09-07 17:08:26 -0300

    split record spec into multiple files

--- spec/holistic/document/unsaved/record/expand_code_spec.rb
+++ spec/holistic/document/unsaved/record/expand_code_spec.rb
@@ -3,22 +3,6 @@
 describe ::Holistic::Document::Unsaved::Record do
   include ::Support::Document::ApplyChange
 
-  describe "#has_unsaved_changes?" do
-    it "returns true if the document has unsaved changes" do
-      document = described_class.new(path: "/example.rb", content: ::String.new("content"))
-
-      expect(document.has_unsaved_changes?).to be(false)
-
-      insert_text_on_document(document:, text: "a", line: 0, column: 0)
-
-      expect(document.has_unsaved_changes?).to be(true)
-
-      document.mark_as_saved!
-
-      expect(document.has_unsaved_changes?).to be(false)
-    end
-  end
-
   describe "#expand_code" do
     let(:content) do
       <<~RUBY
---
+++ spec/holistic/document/unsaved/record/has_unsaved_changes_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+describe ::Holistic::Document::Unsaved::Record do
+  include ::Support::Document::ApplyChange
+
+  describe "#has_unsaved_changes?" do
+    it "returns true if the document has unsaved changes" do
+      document = described_class.new(path: "/example.rb", content: ::String.new("content"))
+
+      expect(document.has_unsaved_changes?).to be(false)
+
+      insert_text_on_document(document:, text: "a", line: 0, column: 0)
+
+      expect(document.has_unsaved_changes?).to be(true)
+
+      document.mark_as_saved!
+
+      expect(document.has_unsaved_changes?).to be(false)
+    end
+  end
+end
