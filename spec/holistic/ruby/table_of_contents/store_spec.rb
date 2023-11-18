e97133a8dfdd3537d0ca9ab63a6e0bb968dda8a7
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-11-18 10:48:59 -0300

    add specs

--- spec/holistic/ruby/table_of_contents/store_spec.rb
+++ spec/holistic/ruby/table_of_contents/store_spec.rb
@@ -34,8 +34,58 @@ describe ::Holistic::Ruby::TableOfContents::Store do
   end
 
   context "when scope already has a table of contents" do
+    it "adds the item to the existing table of contents" do
+      scope = application.scopes.find("::Foo")
+
+      table_of_contents = described_class.call(
+        database: application.database,
+        scope:,
+        identifier: "url",
+        type: ::Holistic::Ruby::TableOfContents::Type::Instance.new("::String")
+      )
+
+      described_class.call(
+        database: application.database,
+        scope:,
+        identifier: "max_retry",
+        type: ::Holistic::Ruby::TableOfContents::Type::Instance.new("::Integer")
+      )
+
+      expect(table_of_contents.items.keys).to eql(["url", "max_retry"])
+
+      table_of_contents.items["url"].tap do |url|
+        expect(url.type.fully_qualified_name).to eql("::String")
+      end
+
+      table_of_contents.items["max_retry"].tap do |max_retry|
+        expect(max_retry.type.fully_qualified_name).to eql("::Integer")
+      end
+    end
   end
 
   context "when scope has table of contents and identifier already exists" do
+    it "overrides the existing item" do
+      scope = application.scopes.find("::Foo")
+
+      table_of_contents = described_class.call(
+        database: application.database,
+        scope:,
+        identifier: "url",
+        type: ::Holistic::Ruby::TableOfContents::Type::Instance.new("::String")
+      )
+
+      described_class.call(
+        database: application.database,
+        scope:,
+        identifier: "url",
+        type: ::Holistic::Ruby::TableOfContents::Type::Instance.new("::URL")
+      )
+
+      expect(table_of_contents.items.keys).to eql(["url"])
+
+      table_of_contents.items["url"].tap do |url|
+        expect(url.type.fully_qualified_name).to eql("::URL")
+      end
+    end
   end
 end
